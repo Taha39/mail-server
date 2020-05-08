739060bf431e53c74e25e96e8953b7a3d6f88e67
Taha39 <mohammad.taha39@gmail.com>
2020-05-08 12:05:34 +0530

    This will handle mail-server, just need non secure google gmail

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Express & Nodemailer
+ 
+#To run: From command prompt "node server.js"
+
+# Server will listen on port: 3210
---
+++ server.js
@@ -0,0 +1,82 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+var nodemailer = require('nodemailer')
+require('dotenv').config();
+var app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+
+// email transporter
+var sender = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+});
+
+
+// route to email post request.
+app.post('/email', (req, res)=>{
+    
+    let name = req.body.name;
+    let email = req.body.email;
+    let contact = req.body.contact;
+    let msg = req.body.msg;
+
+    var mailContent = {
+        from: process.env.EMAIL,
+        to: 'mohammad.taha39@gmail.com',
+        subject: `Hopit support for ${email}`,
+        //text: msg
+        html: `<div>
+                <h4>Name: ${name}</h4></br>
+                <h4>Email: ${email}</h4></br>
+                <h4>Contact: ${contact}</h4></br>
+                <h4>Message: ${msg}</h4>
+              </div>`,
+       
+    }
+
+    sender.sendMail(mailContent, (error)=>{
+        if(error){
+            console.log(error)
+            res.send(error)
+        } else {
+            res.send('success')
+        }
+    })
+})
+
+app.listen(3210, ()=>{
+    console.log('Server Email listning port 3210!')
+})
+
+
+/*
+
+This is for testing 
+var mailContent = {
+    from: process.env.EMAIL,
+    to: 'mohammad.taha39@gmail.com',
+    subject: `Hopit support`,
+    //text: msg
+    html: `<div>
+            <h4>Name: ABC</h4></br>
+            <h4>Email: abc@gmail.com</h4></br>
+            <h4>Contact: 8978787898</h4></br>
+            <h4>Message: Test kar raha hu</h4>
+          </div>`,
+   
+}
+
+sender.sendMail(mailContent, (error)=>{
+    if(error){
+        console.log(error)
+        //res.send(error)
+    } else {
+        res.send('success')
+    }
+})*/
